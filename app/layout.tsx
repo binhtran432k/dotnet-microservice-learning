08994ebab0bc4c735c16c06a0a20984be3aed2d1
Binh Duc Tran <binhtran432k@gmail.com>
2023-12-17 10:42:35 +0700

    prepare for database setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import { inter } from '@/app/ui/fonts';
+import '@/app/ui/global.css';
+import clsx from 'clsx';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
 }
