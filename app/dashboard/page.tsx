cf7dc2ccedaffe9c11256015f5ea601920599c28
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-17 16:48:41 +0700

    feat(dashboard): add latest invoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import clsx from 'clsx';
-import { fetchRevenue } from '../lib/data';
+import { fetchLatestInvoices, fetchRevenue } from '../lib/data';
 import { lusitana } from '../ui/fonts';
 import RevenueChart from '../ui/dashboard/revenue-chart';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={clsx(lusitana.className, 'mb-4 text-xl md:text-2xl')}>
@@ -13,6 +15,7 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
