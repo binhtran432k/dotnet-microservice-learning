6905d516ff75f8c4289d4de01b094cb0417cf49c
Binh Duc Tran <binhtran432k@gmail.com>
2024-01-18 07:45:51 +0700

    feat(dashboard/overview): add loading

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '@/app/lib/data';
+import { Card } from '@/app/ui/dashboard/cards';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
 import clsx from 'clsx';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../lib/data';
-import { Card } from '../ui/dashboard/cards';
-import LatestInvoices from '../ui/dashboard/latest-invoices';
-import RevenueChart from '../ui/dashboard/revenue-chart';
-import { lusitana } from '../ui/fonts';
 
 export default async function Page() {
   const revenue = await fetchRevenue();
